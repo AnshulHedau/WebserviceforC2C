1b37e8f1022debb335ff541452df12f9877ba6ee
AnshulHedau <anshulhedau01@gmail.com>
2017-04-15 02:26:36 +0530

    changes

--- print.php
+++ print.php
@@ -42,9 +42,11 @@ $conn->query($sql);
 
 $sql = "CREATE TABLE admin (name VARCHAR(40) NOT NULL,username VARCHAR(40) NOT NULL, mobno VARCHAR(10) NOT NULL, email VARCHAR(40) NOT NULL ,Password VARCHAR(40) NOT NULL, IFSC VARCHAR(11) NOT NULL , accno VARCHAR(40) NOT NULL, PRIMARY KEY (UserName))";
 
+$sql1 = "Insert into admin values ('Anshul97','Anshul1','940459595',anshul.gmil.com','amshis','sbin1221','02289103')";
 
 
-if ($conn->query($sql) === TRUE) {
+
+/*if ($conn->query($sql) === TRUE) {
 
     echo "Table admin created successfully!";} 
 
@@ -53,7 +55,9 @@ else {
     echo "Error creating table: " . $conn->error;
 
 }
-
+*/
+$conn->query($sql1);
+echo"Yes";
 
 
 //Getting all data from server
