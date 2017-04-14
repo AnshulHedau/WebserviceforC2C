3439b67d5057c5dec50ebab7dcf942779a4207d2
AnshulHedau <anshulhedau01@gmail.com>
2017-04-15 02:33:26 +0530

    commit

--- print.php
+++ print.php
@@ -31,33 +31,30 @@ if ($conn->connect_error) {
 
 
 echo "Connected successfully";
-
-$sql = "DROP TABLE doner";
-
-
-
-$conn->query($sql);
-
-//Create table query
-
-$sql = "CREATE TABLE admin (name VARCHAR(40) NOT NULL,username VARCHAR(40) NOT NULL, mobno VARCHAR(10) NOT NULL, email VARCHAR(40) NOT NULL ,Password VARCHAR(40) NOT NULL, IFSC VARCHAR(11) NOT NULL , accno VARCHAR(40) NOT NULL, PRIMARY KEY (UserName))";
-
-$sql1 = "Insert into admin values ('Anshul','Anshul1','940459595',anshul.gmil.com','anshul','sbin1221','02289103')";
-
-
-
-/*if ($conn->query($sql) === TRUE) {
-
-    echo "Table admin created successfully!";} 
-
-else {
-
-    echo "Error creating table: " . $conn->error;
-
-}
-*/
-$conn->query($sql1);
 echo"Yes";
+$sql = "SELECT * FROM admin";
+
+
+
+    $result = mysqli_query($conn ,$sql);
+
+    $array = [];
+
+    
+
+    while($row = $result->fetch_assoc()){
+
+        $array[] = $row;
+
+    }
+
+    
+
+    header('Content-Type:Application/json');
+
+    echo json_encode($array);
+
+    mysqli_free_result($result);
 
 
 //Getting all data from server
