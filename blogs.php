9c5c2689d1c711bfbad38bc2333dc9d2f5a21ad1
AnshulHedau <anshulhedau01@gmail.com>
2017-04-15 05:14:14 +0530

    New blog table

---
+++ blogs.php
@@ -0,0 +1,55 @@
+<?php
+
+
+
+//Login details
+
+$servername = "mysql4.gear.host";
+
+$username = "bloodbank";
+
+$password = "Wk602y_N66F_";
+
+$dbname = "bloodbank";
+
+
+
+//Create connection
+
+$conn = new mysqli("$servername", $username, $password, $dbname);
+
+
+
+//Check connection
+
+if ($conn->connect_error) {
+
+    die("Connection failed: " . $conn->connect_error);
+
+} 
+
+
+
+echo "Connected successfully";
+
+
+
+//Create table query
+
+$sql = "CREATE TABLE blogs (title VARCHAR(40) NOT NULL,author VARCHAR(40) NOT NULL, hashtags VARCHAR(10) NOT NULL, paymentType VARCHAR(40) NOT NULL ,price NUMBER(3) , description VARCHAR(120) NOT NULL , body VARCHAR(120) NOT NULL,viewers NUMBER(5),date VARCHAR(10),time VARCHAR(10), PRIMARY KEY (title))";
+
+
+
+if ($conn->query($sql) === TRUE) {
+
+    echo "Table blogs created successfully!";} 
+
+else {
+
+    echo "Error creating table: " . $conn->error;
+
+}
+
+
+
+?>
