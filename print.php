35866873348b99b090a721d8e8126ca4fc50aeb5
AnshulHedau <anshulhedau01@gmail.com>
2017-04-15 01:31:21 +0530

    Bug fixed

--- print.php
+++ print.php
@@ -1,6 +1,87 @@
 <?php
 
 
+<?php
 
-echo "Hello user";
+
+
+//Login details
+
+$servername = "mysql4.gear.host";
+
+$username = "bloodbank";
+
+$password = "Wk602y_N66F_";
+
+$dbname = "bloodbank";
+
+
+
+//Create connection
+
+$conn = new mysqli("$servername", $username, $password, $dbname);
+
+
+
+//Check connection
+
+//if ($conn->connect_error) {
+
+//    die("Connection failed: " . $conn->connect_error);
+
+//} 
+
+
+
+//echo "Connected successfully";
+
+
+
+//Create table query
+
+//$sql = "CREATE TABLE admin (name VARCHAR(40) NOT NULL,username VARCHAR(40) NOT NULL, mobno VARCHAR(10) NOT NULL, email VARCHAR(40) NOT NULL ,Password VARCHAR(40) NOT NULL, IFSC VARCHAR(11) NOT NULL , accno VARCHAR(40) NOT NULL, PRIMARY KEY (UserName))";
+
+
+
+//if ($conn->query($sql) === TRUE) {
+
+//    echo "Table BloodBank created successfully!";} 
+
+//else {
+
+//    echo "Error creating table: " . $conn->error;
+
+//}
+
+
+
+//Getting all data from server
+
+//Sample URL call is : http://localhost/PHP_Tutorial/web_service.php?admin
+
+
+    $sql = "SELECT * FROM admin";
+
+
+
+    $result = mysqli_query($conn ,$sql);
+
+    $array = [];
+
+    
+
+    while($row = $result->fetch_assoc()){
+
+        $array[] = $row;
+
+    }
+
+    
+
+    header('Content-Type:Application/json');
+
+    echo json_encode($array);
+
+    mysqli_free_result($result);
+
 ?>
