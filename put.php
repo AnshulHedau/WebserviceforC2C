2ca0c072c9bbee95c86c4fa9311ea9e075d8af3d
AnshulHedau <anshulhedau01@gmail.com>
2017-04-15 02:38:39 +0530

    add files

---
+++ put.php
@@ -0,0 +1,66 @@
+<?php
+
+
+
+//Login details
+
+$servername = "mysql4.gear.host";
+
+$username = "bloodbank";
+
+$password = "Wk602y_N66F_";
+
+$dbname = "bloodbank";
+
+
+
+Create connection
+
+$conn = new mysqli("$servername", $username, $password, $dbname);
+
+
+
+Check connection
+
+if ($conn->connect_error) {
+
+    die("Connection failed: " . $conn->connect_error);
+
+} 
+
+
+
+echo "Connected successfully";
+
+if(isset($_GET['type'])){
+
+    $name = $_GET['name'];
+
+    $username = $_GET['username'];
+
+    $mobno = $_GET['mobno'];
+
+    $email = $_GET['email'];
+
+    $password = $_GET['password'];
+
+    $IFSC = $_GET['IFSC'];
+    
+    $accno = $_GET['accno'];
+    
+    $sql = "INSERT INTO admin (name, username, mobno  , email, password, IFSC,accno) VALUES ($name, $username, $mobno  , $email, $password, $IFSC,$accno)";
+
+
+
+    if ($conn->query($sql) === TRUE) {
+
+        echo "You have been registered successfully!";
+
+    } else {
+
+        echo "Error: " . $sql . "<br>" . $conn->error;
+
+    }
+}
+?>
+
