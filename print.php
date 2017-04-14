d224e38f37b5e367c2d8414a265da7a97af5dd0d
AnshulHedau <anshulhedau01@gmail.com>
2017-04-15 02:22:54 +0530

    commit

--- print.php
+++ print.php
@@ -1,18 +1,16 @@
-
-
 <?php
 
 
 
 //Login details
 
-$servername = "mysql4.gear.host";
+$servername = "mysql3.gear.host";
 
-$username = "bloodbank";
+$username = "adminanshul";
 
-$password = "Wk602y_N66F_";
+$password = "@123456";
 
-$dbname = "bloodbank";
+$dbname = "softwaredb";
 
 
 
@@ -22,7 +20,7 @@ $conn = new mysqli("$servername", $username, $password, $dbname);
 
 
 
-Check connection
+//Check connection
 
 if ($conn->connect_error) {
 
@@ -34,51 +32,29 @@ if ($conn->connect_error) {
 
 echo "Connected successfully";
 
+$sql = "DROP TABLE doner";
 
+
+
+$conn->query($sql);
 
 //Create table query
-$sql = "CREATE TABLE admin (name VARCHAR(40) NOT NULL,username VARCHAR(40) NOT NULL, mobno VARCHAR(10) NOT NULL, email VARCHAR(40) NOT NULL ,Password VARCHAR(40) NOT NULL, IFSC VARCHAR(11), accno VARCHAR(40), PRIMARY KEY (UserName))";
+
+$sql = "CREATE TABLE admin (name VARCHAR(40) NOT NULL,username VARCHAR(40) NOT NULL, mobno VARCHAR(10) NOT NULL, email VARCHAR(40) NOT NULL ,Password VARCHAR(40) NOT NULL, IFSC VARCHAR(11) NOT NULL , accno VARCHAR(40) NOT NULL, PRIMARY KEY (UserName))";
 
 
 
 if ($conn->query($sql) === TRUE) {
 
-    echo "Table BloodBank created successfully!";} 
+    echo "Table admin created successfully!";} 
 
 else {
 
-   echo "Error creating table: " . $conn->error;
+    echo "Error creating table: " . $conn->error;
 
 }
 
 
 
 //Getting all data from server
-
-//Sample URL call is : http://localhost/PHP_Tutorial/web_service.php?admin
-
-
-  /*  $sql = "SELECT * FROM admin";
-    echo "Helo";
-
-    $result = mysqli_query($conn ,$sql);
-
-    $array = [];
-
-    
-
-    while($row = $result->fetch_assoc()){
-
-        $array[] = $row;
-
-    }
-
-    
-
-    header('Content-Type:Application/json');
-
-    echo json_encode($array);
-
-    mysqli_free_result($result);*/
-
 ?>
