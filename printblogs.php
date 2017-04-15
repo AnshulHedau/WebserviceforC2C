c8969644663cb781b5b3b6b359580cb2f6da2158
AnshulHedau <anshulhedau01@gmail.com>
2017-04-15 06:19:01 +0530

    read blogs

---
+++ printblogs.php
@@ -0,0 +1,61 @@
+<?php
+
+
+
+//Login details
+
+$servername = "mysql3.gear.host";
+
+$username = "adminanshul";
+
+$password = "@123456";
+
+$dbname = "softwaredb";
+
+
+
+//Create connection
+
+$conn = new mysqli("$servername", $username, $password, $dbname);
+
+
+
+//Check connection
+
+if ($conn->connect_error) {
+
+    die("Connection failed: " . $conn->connect_error);
+
+} 
+
+
+
+echo "Connected successfully";
+echo"Yes";
+$sql = "SELECT * FROM blogs";
+
+
+
+    $result = mysqli_query($conn ,$sql);
+
+    $array = [];
+
+    
+
+    while($row = $result->fetch_assoc()){
+
+        $array[] = $row;
+
+    }
+
+    
+
+    header('Content-Type:Application/json');
+
+    echo json_encode($array);
+
+    mysqli_free_result($result);
+
+
+//Getting all data from server
+?>
