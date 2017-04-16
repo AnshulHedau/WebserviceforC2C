062b9dcaed0c5b899e46bb99e6815fe2b55f3252
AnshulHedau <anshulhedau01@gmail.com>
2017-04-16 10:29:19 +0530

    Update cric.php

--- cric.php
+++ cric.php
@@ -1,8 +1,16 @@
-<?php
+<html>
+	<head>
+	</head>
+	<?php
 
 	$cricketMatchesTxt = file_get_contents('http://cricapi.com/api/cricket/?apikey=7dn28he8h2e72');	// change with your API key
 	$cricketMatches = json_decode($cricketMatchesTxt);
 
     foreach($cricketMatches->data as $item) {
-php echo($item->title); 
-} ?>
+	?>
+	<body>
+	<h4><?php echo($item->title); ?></h4>
+	</body>
+</html>
+
+<?php } ?>
